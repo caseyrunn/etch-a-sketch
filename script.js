73060cbac35e61e192cc0cd8982c7db07411c8d5
caseyrunn <caseyrunn@gmail.com>
2023-09-24 11:05:56 -0600

    Create New Grid button and logic

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 let container = document.querySelector('.container');
 
-for (let i = 0; i < 16; i ++){
-    let rowDiv = document.createElement('div');
-    rowDiv.classList.add('rowDiv');
-    for(let i = 0; i < 16; i ++){
-        let columnDiv = document.createElement('div');
-        columnDiv.classList.add('columnDiv');
-        columnDiv.addEventListener('mouseenter', function(){
-            columnDiv.classList.add('colorAdded');
-        });
-        rowDiv.appendChild(columnDiv);
-    }
-    container.appendChild(rowDiv);
+window.addEventListener("load", buildGrid(16))
 
+function buildGrid(gridNumber){
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+    for (let i = 0; i < gridNumber; i ++){
+        let rowDiv = document.createElement('div');
+        rowDiv.classList.add('rowDiv');
+        for(let i = 0; i < gridNumber; i ++){
+            let columnDiv = document.createElement('div');
+            columnDiv.classList.add('columnDiv');
+            columnDiv.addEventListener('mouseenter', function(){
+                columnDiv.classList.add('colorAdded');
+            });
+            rowDiv.appendChild(columnDiv);
+        }
+        container.appendChild(rowDiv);
+    }
 }
+
+function makeNewGrid(){
+    let gridNumber = getGridNumber();
+    buildGrid(gridNumber);
+}
+
+function getGridNumber(){
+    return prompt("How many squares?");
+}
+
+let newGridButton = document.querySelector('.button')
+
+newGridButton.addEventListener('click', makeNewGrid)
