d8082e2472ee1f1667047a07406590245377fb0c
caseyrunn <caseyrunn@gmail.com>
2023-09-24 10:11:33 -0600

    Add mouseenter event listener and colorAdded class

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ for (let i = 0; i < 16; i ++){
     for(let i = 0; i < 16; i ++){
         let columnDiv = document.createElement('div');
         columnDiv.classList.add('columnDiv');
+        columnDiv.addEventListener('mouseenter', function(){
+            columnDiv.classList.add('colorAdded');
+        });
         rowDiv.appendChild(columnDiv);
     }
     container.appendChild(rowDiv);
+
 }
