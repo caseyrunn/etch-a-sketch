0e70f063d96045bba64e21be8b08b3f639b6f050
caseyrunn <caseyrunn@gmail.com>
2023-09-23 18:04:06 -0600

    Add 16 divs inside of each rowDiv

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let container = document.querySelector('.container');
 console.log(container);
 
 for (let i = 0; i < 16; i ++){
-    let newDiv = document.createElement('div');
-    container.appendChild(newDiv);
+    let rowDiv = document.createElement('div');
+    for(let i = 0; i < 16; i ++){
+        let columnDiv = document.createElement('div');
+        rowDiv.appendChild(columnDiv);
+    }
+    container.appendChild(rowDiv);
 }
